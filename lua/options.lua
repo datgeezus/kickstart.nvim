d77701307c5be096ec909be61f2c9d4f30915035
Jesus Sanchez <jesus.san.sev@gmail.com>
2024-06-10 11:50:20 -0400

    enable custom

--- lua/options.lua
+++ lua/options.lua
@@ -5,7 +5,6 @@ vim.opt.softtabstop = 4 -- number of spaces in tab when editting
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true -- tabs as spaces
 vim.opt.smartindent = true
-vim.opt.fileencoding = 'utf-8'
 vim.opt.showmatch = true -- highlight matching [{()}]
 vim.opt.incsearch = true -- search as charactes are entered
 vim.opt.swapfile = false -- creates a swap file
